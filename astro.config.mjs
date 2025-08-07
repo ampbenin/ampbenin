f4648c47dbc925fc7a764b039db6e20623a99b0f
ampbenin <ampoddbenin@gmail.com>
2025-08-07 11:27:47 +0100

    Nettoyage: suppression de @astrojs/image et utilisation des assets natifs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
-    image(),
+    tailwind(), 
   ],
-  site: "https://ampbenin.org", // tu peux mettre localhost si besoin
+  site: "https://ampbenin.org", // ou "http://localhost:4321" en local
   output: "static",
 
   // Options globales (hors intégrations)
